1302970f374a39ed324ac26ee1c5545c3ed03dbe
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-08-01 21:45:38 -0300

    Add ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import '@controllers/UsersController';
+import express from 'express'
+import '@controllers/UsersController'
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-  return res.json({ message: 'Hello Rocketseat!' });
+  return res.json({ message: 'Hello Rocketseat!' })
 })
 
-app.listen(5000);
+app.listen(5000)
